cfc6fdc182eb5098ca74e72f1476c05b5e5c143a
Charu Priya <priya.charu147@gmail.com>
2015-02-19 13:13:45 +0530

    resolved errors

--- app/js/insert.js
+++ app/js/insert.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-'use strict';
-var insertPage = (function() {
-=======
 app.insertPage = (function() {
->>>>>>> 30b77e420a5771c4b7031ce4c3b67ca596aa91e2
-
+  'use strict';
   var insertIntoMongo = function(data) {
     console.log(data);
 
@@ -26,26 +21,14 @@ app.insertPage = (function() {
       xhr.send(JSON.stringify(data));
     });
 
-<<<<<<< HEAD
-    promise.then(function(response) {
-      console.log(response);
-    }, function(status) {
-      console.log('Unsuccessful!! Error status: ' + status);
-    });
-  };
-  return {
-    insertIntoMongo: insertIntoMongo
-  };
-=======
         promise.then(function(response) {
           console.log(response);
         }, function(status) {
-            alert("Unsuccessful!! Error status: " + status);
+            console.log('Unsuccessful!! Error status: ' + status);
         });
     };
 
     return {
         insertIntoMongo: insertIntoMongo
     };
->>>>>>> 30b77e420a5771c4b7031ce4c3b67ca596aa91e2
 })();
